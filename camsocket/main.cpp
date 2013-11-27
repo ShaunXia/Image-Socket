532c6050051261cdb2b25bf8556049514fad4503
ShaunXia <shaun.xia@outlook.com>
2013-11-27 16:53:50 +0800

    Initial Commit

---
+++ camsocket/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  camsocket
+//
+//  Created by Shaun on 13-11-27.
+//  Copyright (c) 2013年 Shaun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
